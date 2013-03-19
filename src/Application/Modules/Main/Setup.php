29701f749a4343eff93090b4ca0c374696c2777a
TheSavior <github@eli-white.com>
2013-03-19 02:39:49 -0700

    Moving to the new fitbit library

--- src/Application/Modules/Main/Setup.php
+++ src/Application/Modules/Main/Setup.php
@@ -35,9 +35,12 @@ class Setup
 	{
         session_start();
 
-        $serviceFactory = new \OAuth\ServiceFactory();
-
         $storage = new \Application\Classes\OAuth\Storage\SarosSession();
+
+
+        $registry->fitbit = new \Fitbit\Api($_SERVER["FITBIT_CLIENT"], $_SERVER["FITBIT_SECRET"], $registry->utils->makeLink("Login", "callback"), "json", $storage);
+
+        $serviceFactory = new \OAuth\ServiceFactory();
 
         // Setup the credentials for the requests
         $credentials = new \OAuth\Common\Consumer\Credentials(
